6cddff6641903d5b12b4b68f20bff9373064d39f
marioarau <49352834+marioarau@users.noreply.github.com>
2019-07-15 11:18:58 -0700

    Add files via upload
    setup of game complete need to process answers and score

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,27 +1,76 @@
 
-
-var trivia = {
-	question: 
-		["What is an array?",
-		"How do you access an array element?",
-		"What is the index value of the first element?"],
-	answers: 
+var question = [
+		"What is an array?", 		
+		"What are Kirin, Sapporo, Asahi, and Suntory?",
+		"When Mt. Pelee erupted it killed 40,000 people. On what island is Mt. Pelee located?"	
+	];
+	
+var answers = [
 		["A random integer",
 		"A program that you can use to execute another program and anlyze its run-time behavior",
-		"A sequence of values"]
-		["By the name of the array followed by the index value in brackets",
-		"By the name of the array followed by a dot and then the index value",
-		"By the index value followed by a dot and then the name of the array"]
-		["-1","0","1","2"]
-}
+		"A sequence of values"],
+		["four Japanese car makers",
+		"four dangerous creatures of mythology",
+		"four brands of Japanese beer"],
+		["Hawaii","Java","Martinique"]
+	];
 
-function init() {
+	function init() {
 	countdown = 60; // seconds
-	$("#start-button").hide();
-	start_timer();
+	$("#start-button").show();
+	$("#finished-button").hide();
+	//$("#start-button").hide();
+	$("#questions").hide();
    
-   //$(".img1A").show();
-   //console.log("score[0]="+score[0]);
+   console.log("init function finished");
+};
+
+function start() {
+
+	var answerText = "";
+	$("#start-button").hide();
+	$("#questions").show();
+	questionLength = question.length+1;
+	j = 1;
+	for (i=1; i < question.length+1; i++) {
+		var divQuestion = $("<div>");
+		//divQuestion.attr("data-name", question[i]);
+		divQuestion.attr("id", "question"+i);
+		divQuestion.text(question[i-1]);
+		$("#questions").append(divQuestion);
+		$("#question"+i).show();
+		console.log("#question"+i+"="+question[i]);
+		//console.log(answers[0].length)
+		var divAnswers = $("<div>");
+		//divQuestion.attr("data-name", question[i]);
+		divAnswers.attr("id", "answers"+i);
+		$("#question"+i).append(divAnswers);
+		$("#answers"+i).show();
+		for (j=1; j<answers[0].length+1; j++) {
+			var inputTag = $("<input>");
+			inputTag.attr("type","radio");
+			inputTag.attr("id", "answer"+i+j);
+			inputTag.attr("name", "answer"+i+j);
+			$("#answers"+i).append(inputTag);
+			var spanTag = $("<span>");
+			spanTag.attr("class", ".answer");
+			spanTag.text(answers[i-1][j-1]);
+			$("#answers"+i).append(spanTag);
+			$("#answers"+i).show();
+			//var divAnswer = $("<div>");
+			//divAnswer.attr("id", "answerText");
+			//divAnswer.text(answers[i-1][j-1]);
+			//$("#answer"+i+j).append(divAnswer);
+			//answerText = answers[i-1][j-1];
+			console.log("answerText="+answers[i-1][j-1]);
+			//$(answer).text(answers[i-1][j-1]);
+		}
+		$("#finished-button").show();
+
+	}
+	countdown = 600; // 
+	start_timer();
+	console.log("start function finished");
 };
 
 function times_up() {
@@ -31,7 +80,7 @@ function times_up() {
 function start_timer() {
 	timer_interval = 1000; // 1 second
 	var timeInterval = setInterval(function () {
-        console.log('it works' + new Date());
+        //console.log('it works' + new Date());
 		countdown = countdown - 1;
 		$("#countDown").text("Time Remaining: "+countdown+" Seconds");
 		if (countdown == 0) {
@@ -42,8 +91,9 @@ function start_timer() {
 
 };
 
+init();
 $('#start-button').click(function(){
 
-init();
+start();
 
 });
